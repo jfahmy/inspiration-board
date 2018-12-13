fd8dbf5735b9db18aa2860f47b9ad5a052afc445
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-12-13 14:00:52 -0800

    Add snapshot tests for Card and NewCardForm

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,19 +7,60 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
+
+const URL = 'https://inspiration-board.herokuapp.com/boards/JackieFInspo/cards'
+
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      cards: [],
-    };
+      cards: []
+      }
+  }
+
+
+  componentDidMount() {
+    axios.get(URL)
+      .then((response) => {
+        const cardSet = response.data.map((card) => {
+          console.log(card)
+          const newCard = {
+            ...card.card
+          };
+          console.log(newCard)
+          return newCard;
+        })
+
+        this.setState({
+          cards: cardSet
+        });
+
+      })
+      .catch((error) => {
+        console.log(error.message);
+        this.setState({
+          errorMessage: error.message,
+        });
+      });
   }
 
   render() {
+    console.log(this.state.cards)
+    const allCards = this.state.cards
+      .map((card, i) => {
+        return (
+          <Card
+            key={i}
+            id={i}
+            text={card.text}
+            emoji = {card.emoji}
+          />
+        )
+      })
     return (
-      <div>
-        Board
+      <div className="board">
+        {allCards}
       </div>
     )
   }
@@ -27,7 +68,9 @@ class Board extends Component {
 }
 
 Board.propTypes = {
-
+  cards: PropTypes.array,
+  url: PropTypes.string,
+  boardName: PropTypes.string
 };
 
 export default Board;
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,18 +4,29 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
+
 class Card extends Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
+    console.log(this.props.text)
     return (
       <div className="card">
-        Card
+        <div className="card__content">
+        <h2 className="card__content-text"> {this.props.text} </h2>
+        <p className="card__content-emoji">{emoji.getUnicode("heart_eyes")}</p>
+        </div>
       </div>
     )
   }
 }
 
 Card.propTypes = {
-
+  text: PropTypes.string
 };
 
 export default Card;
